3d68f30fc9e5238e26df97dfe555ceb12cfb7fe0
Pvttuang <ratjagorn.bo.64@ubu.ac.th>
2023-07-30 15:43:55 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+var express = require('express')
+var cors = require('cors')
+const mysql = require('mysql2');
+require('dotenv').config()
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
+});
+
+var app = express()
+
+app.use(cors())
+
+app.get('/helloword', function (req, res, next) {
+  res.json({msg: 'HelloWorld'})
+})
+
+app.get('/users' , function(req,res,next){
+    connection.query(
+        'SELECT * FROM `users`',
+        function(err, results, fields) {
+         res.json(results)                                                        
+        }
+    );
+})
+
+
+
+app.listen(5000, function () {
+  console.log('CORS-enabled web server listening on port 5000')
+})
